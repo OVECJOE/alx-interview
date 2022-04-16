b9d1708bb0231e5973fd40751cd07707db484bfd
OVECJOE <ovecjoe123@gmail.com>
2022-04-16 07:25:16 +0100

    Interview 0x01 successfully solved!; I hope

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -14,16 +14,29 @@ Write a method that determines if all the boxes can be opened.
 '''
 
 
+# def canUnlockAll(boxes):
+#
+#     def updater(seq, box): return seq.extend([key for key in box
+#                                               if (0 < key < len(boxes)) and key not in seq])
+#     valid_keys = []
+#     count = 1
+
+#     updater(valid_keys, boxes[0])
+#     for key in valid_keys:
+#         updater(valid_keys, boxes[key])
+#         count += 1
+
+#     return True if count == len(boxes) else False
+
 def canUnlockAll(boxes):
     '''Determines if all the boxes can be opened'''
-    def updater(seq, box): return seq.extend([key for key in box
-                                              if 0 < key < len(boxes) and key not in seq])
-    valid_keys = []
-    count = 1
-
-    updater(valid_keys, boxes[0])
-    for key in valid_keys:
-        updater(valid_keys, boxes[key])
-        count += 1
-
-    return True if count == len(boxes) else False
+    unlocked = [0]
+    for box_id, box in enumerate(boxes):
+        if not box:
+            continue
+        for key in box:
+            if key < len(boxes) and key not in unlocked and key != box_id:
+                unlocked.append(key)
+    if len(unlocked) == len(boxes):
+        return True
+    return False
